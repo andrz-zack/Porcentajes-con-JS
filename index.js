a2fbf141a5ba18422d34fe11d725237ebacaaa1c
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-03 15:00:08 -0500

    Realice la function calcularPrecioConDescuento

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const inputPrice = document.querySelector('#price');
+const inputDescuento = document.querySelector('#descuento');
 const btn = document.querySelector('#calcular');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
-function calcularPrecioConDescuento {
+function calcularPrecioConDescuento() {
+    //formular de porcentaje (P * (100 - D) / 100) --> P=precio, D=descuento
+
+const precio = inputPrice.value;
+const precioDescuento = inputDescuento.value;
+
+const newPrice = (price * (100 - descuento)) / 100;
 
 }
