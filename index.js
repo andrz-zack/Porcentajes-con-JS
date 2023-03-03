ae68680b75e4892aa70c2307946bf7b8b18029aa
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-03 15:28:43 -0500

    realizando detalles sobre si se pasan el desceunto del 100

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ btn.addEventListener('click', calcularPrecioConDescuento);
 function calcularPrecioConDescuento() {
     //formular de porcentaje (P * (100 - D) / 100) --> P=precio, D=descuento
 
-const price = inputPrice.value;
-const descuento = inputDescuento.value;
+
+const price = Number(inputPrice.value);
+const descuento = Number(inputDescuento.value);
+/* para ver lo que esta sucediendo al colocar los numeros en price y descuento
+console.log({price, descuento}) */
+
+if(!price || !descuento){
+    pResult.innerText = 'Usuario, por favor llena el formulario'
+    return;
+}
+
+if(descuento > 100){
+    pResult.innerText = 'No puedo haber descuento'
+    return;
+}
+
 
 const newPrice = (price * (100 - descuento)) / 100;
 
