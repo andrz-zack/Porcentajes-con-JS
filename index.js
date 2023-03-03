27470066b61e51c6b3f5039eaad7d80f005f3c26
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-03 15:16:16 -0500

    corregi el error que había en las const price y descuento

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const inputPrice = document.querySelector('#price');
 const inputDescuento = document.querySelector('#descuento');
 const btn = document.querySelector('#calcular');
+const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
 function calcularPrecioConDescuento() {
     //formular de porcentaje (P * (100 - D) / 100) --> P=precio, D=descuento
 
-const precio = inputPrice.value;
-const precioDescuento = inputDescuento.value;
+const price = inputPrice.value;
+const descuento = inputDescuento.value;
 
 const newPrice = (price * (100 - descuento)) / 100;
 
-}
+pResult.innerText = 'El nuevo precio es: $ ' + newPrice;
+
+};
